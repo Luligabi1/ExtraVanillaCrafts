935189577428f4adf0ade0cd28816629a60b222c
Luligabi1 <luligabi111@gmail.com>
2021-04-20 22:33:42 -0300

    Initial commit

---
+++ src/main/java/me/luligabi/extravanillacrafts/ExtraVanillaCrafts.java
@@ -0,0 +1,10 @@
+package me.luligabi.extravanillacrafts;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExtraVanillaCrafts implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/luligabi/extravanillacrafts/ExtraVanillaCraftsClient.java
@@ -0,0 +1,13 @@
+package me.luligabi.extravanillacrafts;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ExtraVanillaCraftsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
